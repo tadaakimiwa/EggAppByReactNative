d463d4006b1e693e7d6916195dd71a948b659336
三輪忠暉 <miwatadaaki@miwanotadashiakiranoMacBook-Pro.local>
2020-05-04 17:22:49 +0900

    Create Bottom Tab

--- App.js
+++ App.js
@@ -1,17 +1,71 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Icon } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 import Appbar from './src/components/Appbar';
-import SignupScreen from './src/screens/SignupScreen';
-import Footerbar from './src/components/Footerbar';
+import AthListScreen from './src/screens/AthListScreen';
+import SearchScreen from './src/screens/SearchScreen';
+import TrendListScreen from './src/screens/TrendListScreen';
+import UserPageScreen from './src/screens/UserPageScreen';
+
+const MaterialTab = createMaterialBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <NavigationContainer style={styles.container}>
       <Appbar />
-      <SignupScreen />
-      <Footerbar />
-    </View>
+      <MaterialTab.Navigator
+        activeColor="#e91e63"
+        style={{ backgroundColor: 'tomato' }}
+      >
+        <MaterialTab.Screen
+          name="Home"
+          component={AthListScreen}
+          options={{
+            tabBarColor: '#0e141d',
+            tabBarLabel: 'Athletes',
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="run" color={color} size={26} />
+            ),
+          }}
+        />
+        <MaterialTab.Screen
+          name="Search"
+          component={SearchScreen}
+          options={{
+            tabBarColor: '#281b39',
+            tabBarLabel: 'Search',
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="magnify" color={color} size={26} />
+            ),
+          }}
+        />
+        <MaterialTab.Screen
+          name="Alert"
+          component={TrendListScreen}
+          options={{
+            tabBarColor: '#E64A19',
+            tabBarLabel: 'Alert',
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="bell" color={color} size={26} />
+            ),
+          }}
+        />
+        <MaterialTab.Screen
+          name="UserPage"
+          component={UserPageScreen}
+          options={{
+            tabBarColor: '#524365',
+            tabBarLabel: 'My Page',
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="account" color={color} size={26} />
+            ),
+          }}
+        />
+      </MaterialTab.Navigator>
+    </NavigationContainer>
   );
 }
 
